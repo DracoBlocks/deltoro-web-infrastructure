def0d72d5e28800e9fee1f95db6500189c005ae1
Bruno Fondevila Rodriguez <BrunoFondevila@twinsoft.es>
2021-12-27 10:23:52 +0100

    Adding lambdas for payment queue

--- bin/infrastructure.ts
+++ bin/infrastructure.ts
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
-import * as cdk from '@aws-cdk/core';
-import { Config } from '../lib/config';
-import { Website } from '../lib/website';
+import * as cdk from "@aws-cdk/core";
+import { Config } from "../lib/config";
+import { PaymentQueue } from "../lib/payment-queue";
+import { Website } from "../lib/website";
 
 const app = new cdk.App();
 const config = new Config(app, "prod");
 
-new Website(app, 'WebsiteStack', config);
+new Website(app, "WebsiteStack", config);
+
+new PaymentQueue(app, "PaymentQueueStack", config);
---
+++ lib/payment-queue.ts
@@ -0,0 +1,40 @@
+import * as cdk from "@aws-cdk/core";
+import { ApiGatewayToSqs } from "@aws-solutions-constructs/aws-apigateway-sqs";
+import {} from "@aws-cdk/aws-lambda";
+import { Config } from "./config";
+
+export class PaymentQueue extends cdk.Stack {
+  private readonly config: Config;
+
+  constructor(
+    scope: cdk.App,
+    id: string,
+    config: Config,
+    props?: cdk.StackProps,
+  ) {
+    super(scope, id, config.getStackProps(props));
+
+    this.config = config;
+    this.createApiGatewayWithSqs();
+  }
+
+  private createApiGatewayWithSqs() {
+    new ApiGatewayToSqs(this, "PaymentAPIGatewaySQS", {
+      deployDeadLetterQueue: false,
+      allowCreateOperation: true,
+      allowReadOperation: false,
+      allowDeleteOperation: false,
+    });
+  }
+
+  //TODO: need to deploy with infra as code:
+  /*
+    - Lambda authorizer
+    - Lambda mint
+    - SQS Settings
+    - API Gateway Settings
+    - SQS triggering lambda mint
+    - Dead letter queue?
+    - IAM roles / permissions needed for all of this
+  */
+}
