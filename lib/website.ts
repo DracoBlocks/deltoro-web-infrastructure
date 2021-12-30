441b33444b3b7b78ee1a947a8abdf2779ba6d8b5
Bruno Fondevila Rodriguez <BrunoFondevila@twinsoft.es>
2021-12-30 11:26:56 +0100

    Improved caching strategy for the website

--- lib/website.ts
+++ lib/website.ts
@@ -1,14 +1,14 @@
-import * as cdk from "@aws-cdk/core";
-import { Duration } from "@aws-cdk/core";
-import * as s3 from "@aws-cdk/aws-s3";
-import * as s3deploy from "@aws-cdk/aws-s3-deployment";
-import * as cloudfront from "@aws-cdk/aws-cloudfront";
-import * as origins from "@aws-cdk/aws-cloudfront-origins";
-import { Config } from "./config";
-import { ViewerProtocolPolicy } from "@aws-cdk/aws-cloudfront";
 import { Certificate } from "@aws-cdk/aws-certificatemanager";
+import * as cloudfront from "@aws-cdk/aws-cloudfront";
+import { ViewerProtocolPolicy } from "@aws-cdk/aws-cloudfront";
+import * as origins from "@aws-cdk/aws-cloudfront-origins";
 import * as route53 from "@aws-cdk/aws-route53";
 import * as alias from "@aws-cdk/aws-route53-targets";
+import * as s3 from "@aws-cdk/aws-s3";
+import * as s3deploy from "@aws-cdk/aws-s3-deployment";
+import * as cdk from "@aws-cdk/core";
+import { Duration } from "@aws-cdk/core";
+import { Config } from "./config";
 
 export class Website extends cdk.Stack {
   private readonly config: Config;
@@ -43,14 +43,48 @@ export class Website extends cdk.Stack {
   }
 
   private deployWebsite() {
-    new s3deploy.BucketDeployment(this, "MainWebsiteHtmlDeployment", {
-      sources: [s3deploy.Source.asset("../out")],
+    const staticContentFolders = ["_next", "images", "videos"];
+    const staticContentFiles = ["favicon.ico"];
+
+    new s3deploy.BucketDeployment(this, "MainWebsiteMoreDynamicPages", {
+      sources: [
+        s3deploy.Source.asset("../out", {
+          exclude: [...staticContentFolders, ...staticContentFiles],
+        }),
+      ],
+      exclude: [...staticContentFolders, ...staticContentFiles],
       destinationBucket: this.bucket,
       cacheControl: [
         s3deploy.CacheControl.maxAge(Duration.seconds(30)),
         s3deploy.CacheControl.mustRevalidate(),
         s3deploy.CacheControl.noCache(),
         s3deploy.CacheControl.fromString("no-store"),
+      ],
+      prune: true,
+    });
+
+    new s3deploy.BucketDeployment(this, "MainWebsiteStaticContent", {
+      sources: [
+        s3deploy.Source.asset("../out", {
+          exclude: [
+            "**",
+            ...staticContentFolders
+              .map((item) => [
+                "!" + item.replace(/[/]$/, ""),
+                "!" + item.replace(/[/]$/, "") + "/**",
+              ])
+              .reduce((a, b) => {
+                return [...a, ...b];
+              }),
+            ...staticContentFiles.map((item) => "!" + item),
+          ],
+        }),
+      ],
+      destinationBucket: this.bucket,
+      cacheControl: [
+        s3deploy.CacheControl.maxAge(Duration.days(30)),
+        s3deploy.CacheControl.setPublic(),
+        s3deploy.CacheControl.fromString("immutable"),
       ],
       prune: false,
     });
@@ -72,7 +106,6 @@ export class Website extends cdk.Stack {
         maxTtl: Duration.days(365),
         cookieBehavior: cloudfront.CacheCookieBehavior.none(),
         headerBehavior: cloudfront.CacheHeaderBehavior.none(),
-        queryStringBehavior: cloudfront.CacheQueryStringBehavior.allowList("v"),
         enableAcceptEncodingGzip: true,
         enableAcceptEncodingBrotli: true,
       },
